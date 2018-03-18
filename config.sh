ef976c96ff02ac7c11e3f21a3cd1751e02e457ee
limingyao <limingyao668@vip.qq.com>
2018-03-18 21:43:22 +0800

    update code

--- config.sh
+++ config.sh
@@ -8,25 +8,25 @@ TIME_STAMP=$(date +%s)
 
 function is_link()
 {
-	local file_path=$1
-	local tmp_string=$(ls -l ${file_path})
-	local symbol=${tmp_string:0:1}
-	if [ $symbol == 'l' ];then
-		return 1
-	fi
-	return 0
+    local file_path=$1
+    local tmp_string=$(ls -l ${file_path})
+    local symbol=${tmp_string:0:1}
+    if [ $symbol == 'l' ];then
+        return 1
+    fi
+    return 0
 }
 
 function preprocessing()
 {
-	local file_path=$1
-	if [ -e ${file_path} ];then
-		is_link ${file_path}
-		if [ $? -eq 1 ];then
-			rm ${file_path}
-		else
-			mv ${file_path} ${file_path}"."${TIME_STAMP}
-		fi
-	fi
+    local file_path=$1
+    if [ -e ${file_path} ];then
+        is_link ${file_path}
+        if [ $? -eq 1 ];then
+            rm ${file_path}
+        else
+            mv ${file_path} ${file_path}"."${TIME_STAMP}
+        fi
+    fi
 }
 
